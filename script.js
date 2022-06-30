00ec965c5e0dafcc321470efb887e86f444099c2
Martin Ruiz Diaz <martinrd.1606@gmail.com>
2022-06-30 00:50:55 -0300

    correct 3.0

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const pipe = document.querySelector(".pipe-game");
 const game = document.querySelector(".game");
 
 if (window.matchMedia("(max-width: 800px)").matches){
-    game.style.height = '88vh';
+    game.style.height = '85vh';
 }
+
 
 var timeRandom = setInterval(() => {
     console.log(timeRandom);
-    timeRandom = Math.floor((Math.random()* 2) + 1);
+    timeRandom = Math.floor(Math.random()* 2) + 1;
     pipe.style.animation = `pipe-animation ${timeRandom}s infinite linear`;
 }, 2000);
 
@@ -34,32 +35,33 @@ class marioDef {
 
 }
 
-//if (window.matchMedia("(min-width: 800px)")){
+
 
 const loopGame = setInterval(() => {
-        const pipePosition = pipe.offsetLeft;
-        const marioPosition = +window
-            .getComputedStyle(mario)
-            .bottom.replace("px", "");
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window
+        .getComputedStyle(mario)
+        .bottom.replace("px", "");
         
 
 
-        if (pipePosition <= 90 && pipePosition > 0 && marioPosition < 90) {
-        pipe.style.animation = "none";
-        pipe.style.left = `${pipePosition}px`;
+    if (pipePosition <= 80 && pipePosition > 0 && marioPosition < 70)
+        {
+            pipe.style.animation = "none";
+            pipe.style.left = `${pipePosition}px`;
 
-        mario.style.animation = 'mario-die 2s';
+            mario.style.animation = 'mario-die 2s';
 
-        mario.src = "./img/mario-game-over.png";
-        if (window.matchMedia("(max-width: 800px)").matches){
-            mario.style.width = "45px";
-        }
-        else{
-            mario.style.width = "75px";
-        }
-        mario.style.marginLeft = "50px";
-    
-        clearInterval(loopGame);
+            mario.src = "./img/mario-game-over.png";
+            if (window.matchMedia("(max-width: 800px)").matches){
+                mario.style.width = "45px";
+            }
+            else{
+                mario.style.width = "75px";
+            }
+            mario.style.marginLeft = "50px";
+        
+            clearInterval(loopGame);
         
 }}, 10);
 
